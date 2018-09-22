d14a27b39c5afe5fe3fbbd5486471f009d078138
Travis Liu <laoliucn@hotmail.com>
2018-09-22 15:21:21 +0800

    update

--- maxPerimeter.py
+++ maxPerimeter.py
@@ -3,6 +3,7 @@ sample1 = [6, 1, 6, 5, 8, 4]
 sample2 = [2, 20, 7, 55, 1, 33, 12, 4]
 sample3 = [33, 6, 20, 1, 8, 12, 5, 55, 4, 9]
 
+
 def solution(A):
     max_peri = 0
     n = len(A)
@@ -17,9 +18,8 @@ def solution(A):
     else:
         return max_peri
 
+
 print(solution(A))
 print(solution(sample1))
 print(solution(sample2))
 print(solution(sample3))
-
-
