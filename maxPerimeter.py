c269eb1f2e87a9c486f8abe117b7a0a489978a59
Travis Liu <travis.liu@dell.com>
2018-10-12 13:16:51 +0800

    update

--- maxPerimeter.py
+++ maxPerimeter.py
@@ -2,6 +2,8 @@ A = [10, 2, 5, 1, 8, 20]
 sample1 = [6, 1, 6, 5, 8, 4]
 sample2 = [2, 20, 7, 55, 1, 33, 12, 4]
 sample3 = [33, 6, 20, 1, 8, 12, 5, 55, 4, 9]
+sample4 = [1, 2, 3, 4, 5, 10]
+
 
 def solution(A):
     max_peri = 0
@@ -17,9 +19,10 @@ def solution(A):
     else:
         return max_peri
 
+
 print(solution(A))
 print(solution(sample1))
 print(solution(sample2))
 print(solution(sample3))
 
-
+print(solution(sample4))
